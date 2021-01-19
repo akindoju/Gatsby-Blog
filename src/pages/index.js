95c7e019eabfb9ddc02342afeed64a3676c997a4
Akndju <davidd.akindoju@gmail.com>
2021-01-19 14:56:29 +0100

    Added minor styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,17 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
-import { node } from "prop-types"
+import styled from "styled-components"
+
+const BlogLink = styled(Link)`
+  text-decoration: none;
+`
+
+const BlogTitle = styled.h3`
+  margin-bottom: 20px;
+  color: blue;
+`
 
 export default ({ data }) => (
   <Layout>
@@ -14,9 +21,11 @@ export default ({ data }) => (
       <h4>{data.allMarkdownRemark.totalCount}</h4>
       {data.allMarkdownRemark.edges.map(({ node }) => (
         <div key={node.id}>
-          <span>
-            {node.frontmatter.title} - {node.frontmatter.date}
-          </span>
+          <BlogLink to={node.fields.slug}>
+            <BlogTitle>
+              {node.frontmatter.title} - {node.frontmatter.date}
+            </BlogTitle>
+          </BlogLink>
           <p>{node.excerpt}</p>
         </div>
       ))}
@@ -26,7 +35,7 @@ export default ({ data }) => (
 
 export const query = graphql`
   query {
-    allMarkdownRemark {
+    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
       edges {
         node {
           id
@@ -34,6 +43,9 @@ export const query = graphql`
             title
             description
             date
+          }
+          fields {
+            slug
           }
           html
           excerpt
